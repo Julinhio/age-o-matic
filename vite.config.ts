376336a322686e2d930d5b401b5908f7fed04121
Julinhio <julinhio@hotmail.com>
2024-12-06 16:55:30 +0700

    Initial commit - Added Age-O-Matic files

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/age-o-matic/', // set the base URL to your repository name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
